63b54b58f4d773a991800df2da62c0956278c524
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-22 03:24:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,10 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else 
-   array.each_wth_index do |name, index|
-     
-   end
+    array.each_with_index do |name, index|
+     array[index] = "#{index.to_i + 1}. #{name}"
+    end
+  puts "The line is currently: #{array.join}"
 end
 
 def take_a_number(array, name)
