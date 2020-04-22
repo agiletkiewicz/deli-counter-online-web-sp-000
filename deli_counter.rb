89ab10c6d527ac3a8ca537e6208bf8523c79af09
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-22 03:13:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,9 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else 
-   array.each_wth_index do |
+   array.each_wth_index do |name, index|
+     
+   end
 end
 
 def take_a_number(array, name)
